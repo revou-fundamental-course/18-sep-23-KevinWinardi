b7713d17cc27e3df5dc418c6a6fb2363d1375580
Kevin Winardi <kevinwinardi122005@gmail.com>
2023-09-27 21:44:06 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+document.getElementById("button").addEventListener("click",function(){
+    let name = document.forms["content-form"]["your-name"].value;
+    let email = document.forms["content-form"]["email"].value;
+    let interest = document.forms["content-form"]["interest"].value;
+    if(name==""){
+        document.getElementById("name-error").innerText="Nama tidak boleh kosong";
+    } else {
+        document.getElementById("name-error").innerText="";
+    }
+
+    if(email==""){
+        document.getElementById("email-error").innerText="Email tidak boleh kosong";
+    } else {
+        document.getElementById("email-error").innerText="";
+    }
+
+    if(interest==""){
+        document.getElementById("interest-error").innerText="Hobi tidak boleh kosong";
+    } else {
+        document.getElementById("interest-error").innerText="";
+    }
+})
+
+let slideIndex=0;
+showSlides();
+
+function showSlides(){
+    let i;
+    let slides=document.getElementsByClassName("slider");
+    for(i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+    slideIndex++;
+    if (slideIndex>slides.length){slideIndex=1}
+    slides[slideIndex-1].style.display="block";
+    setTimeout(showSlides,3000);
+    
+}
